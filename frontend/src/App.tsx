b8ecced12746e491f343f6d6acefce33df0944fd
KietLe261003 <letuankiet26102003kl@gmail.com>
2025-09-10 11:24:50 +0700

    Fix port socket

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,10 @@ import './App.css';
 function App() {
 	return (
 		<FrappeProvider
-			// siteName={import.meta.env.VITE_SITE_NAME || 'frontend'}
-			// socketPort={import.meta.env.VITE_SOCKET_PORT || 9000}
-			siteName={import.meta.env.VITE_SITE_NAME || 'react.test'}
-			socketPort={import.meta.env.VITE_SOCKET_PORT || 9007}
+			siteName={import.meta.env.VITE_SITE_NAME || 'frontend'}
+			socketPort={import.meta.env.VITE_SOCKET_PORT || 9000}
+			// siteName={import.meta.env.VITE_SITE_NAME || 'react.test'}
+			// socketPort={import.meta.env.VITE_SOCKET_PORT || 9007}
 		>
 			<ThemeProvider
 				attribute="class"
